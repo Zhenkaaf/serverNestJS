0b7f8b41ae105dd59546332f516acbdab33f8e11
Zhenkaaf <zhenkaaf@gmail.com>
2023-11-04 19:01:56 +0200

    nestStart

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,44 @@
+// export class User {
+//  DB table
+// email: string;
+//  password: string;
+//}
+
+import { Category } from 'src/category/entities/category.entity'
+import { Transaction } from 'src/transaction/entities/transaction.entity'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @OneToMany(() => Category, (category) => category.user, {
+    onDelete: 'CASCADE', // удаляем usera и его категории
+  })
+  categories: Category[]
+
+  @OneToMany(() => Transaction, (transaction) => transaction.user, {
+    onDelete: 'CASCADE',
+  })
+  transactions: Transaction[]
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+}
