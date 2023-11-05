319b8e28813fd6943558cfa1deba63a8349fd7d4
Zhenkaaf <zhenkaaf@gmail.com>
2023-11-05 12:06:28 +0200

    registrationArgon2

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common'
+import { UserService } from './user.service'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @UsePipes(new ValidationPipe())
   create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+    return this.userService.create(createUserDto)
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
+  /*   @Get()
+  findOne() {
+    return this.userService.findOne()
   }
-
-  @Get(':id')
+ */
+  /*   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
@@ -30,5 +41,5 @@ export class UserController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
-  }
+  } */
 }
