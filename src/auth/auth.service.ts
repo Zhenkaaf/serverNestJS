56af510fc79ede035d7dff4874672d538414824a
Zhenkaaf <zhenkaaf@gmail.com>
2023-11-05 16:12:46 +0200

    loginAuth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UserService } from './../user/user.service'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import * as argon2 from 'argon2'
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(private readonly userService: UserService) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findOne(email)
+    const passwordIsMatch = await argon2.verify(user.password, password)
+    if (user && passwordIsMatch) {
+      return user
+    }
+    //return null;
+    throw new UnauthorizedException('User or password are incorrect')
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -28,8 +28,12 @@ export class UserService {
     return user
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`
+  async findOne(email: string) {
+    return await this.userRepository.findOne({
+      where: {
+        email,
+      },
+    })
   }
 
   /* findAll() {
