f3b98a1fd6c87d2f919656b2230c4c64938b0b48
Zhenkaaf <zhenkaaf@gmail.com>
2023-12-22 19:10:39 +0200

    myGuard

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -15,6 +15,7 @@ import { CategoryService } from './category.service'
 import { CreateCategoryDto } from './dto/create-category.dto'
 import { UpdateCategoryDto } from './dto/update-category.dto'
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
+import { AuthorGuard } from 'src/guard/author.guard'
 
 @Controller('categories')
 export class CategoryController {
@@ -35,14 +36,14 @@ export class CategoryController {
     return this.categoryService.findAll(+req.user.id)
   }
 
-  @Get(':id')
-  @UseGuards(JwtAuthGuard)
+  @Get(':type/:id')
+  @UseGuards(JwtAuthGuard, AuthorGuard)
   findOne(@Param('id') id: string) {
     return this.categoryService.findOne(+id)
   }
 
-  @Patch(':id')
-  @UseGuards(JwtAuthGuard)
+  @Patch(':type/:id')
+  @UseGuards(JwtAuthGuard, AuthorGuard)
   update(
     @Param('id') id: string, //@Param('id') извлекает значение параметра id не из токена, а из части URL-адреса запроса.
     @Body() updateCategoryDto: UpdateCategoryDto,
@@ -50,8 +51,8 @@ export class CategoryController {
     return this.categoryService.update(+id, updateCategoryDto)
   }
 
-  @Delete(':id')
-  @UseGuards(JwtAuthGuard)
+  @Delete(':type/:id')
+  @UseGuards(JwtAuthGuard, AuthorGuard)
   remove(@Param('id') id: string) {
     return this.categoryService.remove(+id)
   }
---
+++ src/guard/author.guard.ts
@@ -0,0 +1,40 @@
+import { CategoryService } from 'src/category/category.service'
+import { TransactionService } from './../transaction/transaction.service'
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common'
+
+@Injectable()
+export class AuthorGuard implements CanActivate {
+  constructor(
+    private readonly transactionService: TransactionService,
+    private readonly categoryService: CategoryService,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest()
+
+    const { id, type } = request.params
+    let entity
+
+    switch (type) {
+      case 'transaction':
+        entity = await this.transactionService.findOne(id)
+        break
+      case 'category':
+        entity = await this.categoryService.findOne(id)
+        break
+
+      default:
+        throw new NotFoundException('Something went wrong..')
+    }
+
+    const user = request.user // приходит из токена
+    if (entity && user && entity.user.id === user.id) {
+      return true
+    }
+    return false
+  }
+}
